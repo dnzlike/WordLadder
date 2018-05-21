773d67262fab552097aca47167472a06bed10aa0
dnzlike <dnz7993@sjtu.edu.cn>
2018-05-21 11:03:47 +0800

    Update WordLadderApplication.java

--- src/main/java/com/example/demo/WordLadderApplication.java
+++ src/main/java/com/example/demo/WordLadderApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WordLadderApplication  extends SpringBootServletInitializer{
